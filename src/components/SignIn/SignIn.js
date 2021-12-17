8f7cc4b2610c88a9ee0b3ba8714b1721a128a52c
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2021-12-17 16:56:51 -0700

    updating sign-up styling

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './SignIn.css';
+import Nav from '../Nav/Nav';
 
 const SignIn = () => {
 	return (
-		<form>
-			<div className='form-title'>Sign In</div>
-			<input className='form-email' type='email' placeholder='Email'></input>
-			<input
-				className='form-password'
-				type='password'
-				placeholder='Password'></input>
-			<a className='forgot-password-link' href='#'>
-				Forgot Password?
-			</a>
-			<button type='submit' className='form-sign-in-btn'>
-				Sign In
-			</button>
-			<div className='sign-up-link'>
-				Don't have an account? <a href='#'>Sign up for free</a>
+		<div className='sign-in-container'>
+			<Nav />
+			<div className='sign-in-form-container'>
+				<div className='form-title'>Sign In</div>
+				<input className='form-email' type='email' placeholder='Email'></input>
+				<input
+					className='form-password'
+					type='password'
+					placeholder='Password'></input>
+				<a className='forgot-password-link' href='#'>
+					Forgot Password?
+				</a>
+				<button type='submit' className='form-sign-in-btn'>
+					Sign In
+				</button>
+				<div className='sign-up-link'>Don't have an account?</div>
+				<div>
+					<a href='/sign-up'>Sign up for free</a>
+				</div>
 			</div>
-		</form>
+		</div>
 	);
 };
 
