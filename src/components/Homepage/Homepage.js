048eacbf834cbaaa0de3fed4ef5e2fbc9153b8ef
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2021-12-20 17:02:32 -0700

    styling homepage and fixing landing page sign in form

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -5,27 +5,33 @@ import '../Nav/Nav.css';
 
 const Homepage = () => {
 	return (
-		<div>
+		<div className='homepage-container'>
 			<Nav />
-			<div>
-				<div>Homepage</div>
-				<div>Current Warnings</div>
+			<div className='homepage-page-container'>
+				<div className='homepage-title'>Homepage</div>
+				<div className='current-warnings-title'>Current Warnings</div>
 				<img
+					className='current-warnings-img'
 					id='us'
 					src='//forecast.weather.gov/wwamap/png/US.png'
-					usemap='#US_large_imagemap_800_500'></img>
-				<div>Current Surface Analysis</div>
+					usemap='#US_large_imagemap_800_500'
+					alt='current-warnings-img'
+				/>
+				<div className='current-surface-title'>Current Surface Analysis</div>
 				<img
+					className='current-surface-img'
 					src='http://www.wpc.ncep.noaa.gov/noaa/national_forecast.jpg'
 					alt='National Weather Outlook'
 					name='national_forecast'
-					width='515'
-					height='412'
-					border='0'
 					usemap='#m_national_forecast'
-					id='national_forecast'></img>
-				<div>Current Satellite Image</div>
-				<img src='https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/1250x750.jpg' />
+					id='national_forecast'
+				/>
+				<div className='current-satellite-title'>Current Satellite Image</div>
+				<img
+					className='current-satellite-img'
+					src='https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/1250x750.jpg'
+					alt='current-satellite-img'
+				/>
 			</div>
 		</div>
 	);
