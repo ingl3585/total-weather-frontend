8bf5e16d3da784bae6fd3a7c2b0bc09064689caa
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2021-12-22 21:02:20 -0700

    commit before deploy

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Homepage.css';
 import NavBar from '../Nav/NavBar';
-import Pic from '../../images/eeee.png';
 
 const Homepage = () => {
 	const [blog, setBlogs] = useState([]);
